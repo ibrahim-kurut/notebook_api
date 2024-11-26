600401be324cbf87ee9ba0d8101fdc1aff04d34e
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-11-26 11:05:22 +0300

    create notebook model

--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
 
     # TPA
     'rest_framework',
+
+    # My app
+    'account.apps.AccountConfig',
+    'notebook.apps.NotebookConfig',
+
+
 ]
 
 MIDDLEWARE = [
--- notebook/models.py
+++ notebook/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create notebook model
+class Notebook(models.Model):
+    # Add a foreign key to the user model
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notebooks')
+    title = models.CharField(max_length=200, blank=False)
+    content = models.TextField(blank=True, default="___________")  # The content can be left empty
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} written by {self.user.username}"
